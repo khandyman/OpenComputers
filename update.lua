25be69f41544a1bc3875b9fa2b233a356d8516c6
khandyman <timothy.s.wise@gmail.com>
2023-03-13 09:03:43 -0400

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,4 +1,4 @@
 filesystem = require 'filesystem'
 
-filesystem.remove(/home/seedBreeder.lua)
+filesystem.remove("/home/seedBreeder.lua")
 wget "https://raw.githubusercontent.com/khandyman/OpenComputers/main/seedBreeder.lua"
